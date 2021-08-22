1e7d2cfd07ae1b680456f434c1d39fb79604f871
CorneilleEdi <corneilleayao@gmail.com>
2021-08-22 12:46:34 +0530

    feat: add interceptor

--- lib/src/services/posts_service.dart
+++ lib/src/services/posts_service.dart
@@ -1,35 +1,41 @@
-
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:echo/src/services/posts_service_interceptor.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
 
 import '../data/post.dart';
 
 class PostsService {
   final String baseApiUrl = "https://jsonplaceholder.typicode.com/posts";
 
- Future<List<PostResponseModel>> getPosts() async {
+  final Client client = InterceptedClient.build(interceptors: [
+    PostsServiceInterceptor(),
+  ]);
+
+  Future<List<PostResponseModel>> getPosts() async {
     Uri url = Uri.parse(baseApiUrl);
-    final http.Response response =  await http.get(url);
+    final Response response = await client.get(url);
 
-    final rawData  = response.body;
+    final rawData = response.body;
 
-    final decoded =  jsonDecode(rawData).cast<Map<String, dynamic>>();
+    final decoded = jsonDecode(rawData).cast<Map<String, dynamic>>();
 
-    final posts =  decoded.map<PostResponseModel>((json) => PostResponseModel.fromJson(json)).toList();
+    final posts = decoded
+        .map<PostResponseModel>((json) => PostResponseModel.fromJson(json))
+        .toList();
 
     return posts;
   }
 
   Future<PostResponseModel> getPost(int postId) async {
-     Uri url = Uri.parse(baseApiUrl+"/$postId");
-    final http.Response response =  await http.get(url);
+    Uri url = Uri.parse(baseApiUrl + "/$postId");
+    final Response response = await client.get(url);
 
-     final rawData  = response.body;
+    final rawData = response.body;
 
-    final decoded =  jsonDecode(rawData);
+    final decoded = jsonDecode(rawData);
 
     return PostResponseModel.fromJson(decoded);
   }
-  
 }
---
+++ lib/src/services/posts_service_interceptor.dart
@@ -0,0 +1,16 @@
+import 'package:http_interceptor/http_interceptor.dart';
+
+class PostsServiceInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print(data.toString());
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+      print(data.toString());
+      return data;
+  }
+
+}
